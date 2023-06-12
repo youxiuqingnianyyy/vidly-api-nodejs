fed86991de906dfaeee489d195b4b050a42dd247
Yuyan Yang <yxqnyyy@gmail.com>
2023-06-13 00:19:48 +0200

    Add Routes and NavBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import {Redirect, Route, Switch} from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import {NavBar} from "./components/navBar";
 import "./App.css";
+import MovieForm from "./components/movieForm";
 
 class App extends Component {
-  render() {
-    return (
-      <main className="container">
-        <Movies />
-      </main>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar/>
+                <main className="container">
+                    <Switch>
+                        <Route path="/movies/:id" component={MovieForm} />
+                        <Route path="/movies" component={Movies}/>
+                        <Route path="/customers" component={Customers}/>
+                        <Route path="/rentals" component={Rentals}/>
+                        <Route path="/not-found" component={NotFound}/>
+                        <Redirect from="/" exact to="/movies"/>
+                        <Redirect to="/not-found"/>
+                    </Switch>
+                </main>
+            </React.Fragment>
+        )
+    }
 }
 
 export default App;
